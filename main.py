0c739ee561dad2afe2d14f001d4c17366f74e7b8
Jingxian Lin <jingxianlin@yahoo.com>
2017-08-14 16:35:19 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,123 @@
+import os.path
+import tensorflow as tf
+import helper
+import project_tests as tests
+
+
+def load_vgg(sess, vgg_path):
+    """
+    Load Pretrained VGG Model into TensorFlow.
+    :param sess: TensorFlow Session
+    :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
+    :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer3_out)
+    """
+    # TODO: Implement function
+    tf.saved_model.loader.load(sess, ['vgg16'], vgg_path)
+    return sess.graph.get_tensor_by_name('image_input:0'), sess.graph.get_tensor_by_name('keep_prob:0'), sess.graph.get_tensor_by_name('layer3_out:0'), \
+           sess.graph.get_tensor_by_name('layer4_out:0'), sess.graph.get_tensor_by_name('layer7_out:0')
+tests.test_load_vgg(load_vgg, tf)
+
+
+def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
+    """
+    Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
+    :param vgg_layer7_out: TF Tensor for VGG Layer 3 output
+    :param vgg_layer4_out: TF Tensor for VGG Layer 4 output
+    :param vgg_layer3_out: TF Tensor for VGG Layer 7 output
+    :param num_classes: Number of classes to classify
+    :return: The Tensor for the last layer of output
+    """
+    # TODO: Implement function
+    new3 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, strides = 1, padding = 'same')
+    new4 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, strides = 1, padding = 'same')
+    new7 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, strides = 1, padding = 'same')
+    tmp1 = tf.add(new4, tf.layers.conv2d_transpose(new7, num_classes, 16, strides = 2, padding = 'same'))
+    tmp2 = tf.add(new3, tf.layers.conv2d_transpose(tmp1, num_classes, 16, strides = 2, padding = 'same'))
+    return tf.layers.conv2d_transpose(tmp2, num_classes, 64, strides = 8, padding = 'same')
+tests.test_layers(layers)
+
+
+def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
+    """
+    Build the TensorFLow loss and optimizer operations.
+    :param nn_last_layer: TF Tensor of the last layer in the neural network
+    :param correct_label: TF Placeholder for the correct label image
+    :param learning_rate: TF Placeholder for the learning rate
+    :param num_classes: Number of classes to classify
+    :return: Tuple of (logits, train_op, cross_entropy_loss)
+    """
+    # TODO: Implement function
+    logits = tf.reshape(nn_last_layer, [-1, num_classes])
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+    train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
+    return logits, train_op, cross_entropy_loss
+tests.test_optimize(optimize)
+
+
+def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+             correct_label, keep_prob, learning_rate):
+    """
+    Train neural network and print out the loss during training.
+    :param sess: TF Session
+    :param epochs: Number of epochs
+    :param batch_size: Batch size
+    :param get_batches_fn: Function to get batches of training data.  Call using get_batches_fn(batch_size)
+    :param train_op: TF Operation to train the neural network
+    :param cross_entropy_loss: TF Tensor for the amount of loss
+    :param input_image: TF Placeholder for input images
+    :param correct_label: TF Placeholder for label images
+    :param keep_prob: TF Placeholder for dropout keep probability
+    :param learning_rate: TF Placeholder for learning rate
+    """
+    # TODO: Implement function
+    sess.run(tf.global_variables_initializer())
+    for i in range(epochs):
+        cnt = 0
+        batches = get_batches_fn(batch_size)
+        for batch in batches:
+            print(i, cnt)
+            cnt += 1
+            _, loss_value = sess.run([train_op, cross_entropy_loss], 
+                {input_image: batch[0], correct_label: batch[1], learning_rate: 1e-4, keep_prob: 0.75})
+        print('Step: %d Loss: %.2f' % (i, loss_value))
+    pass
+tests.test_train_nn(train_nn)
+
+
+def run():
+    num_classes = 2
+    image_shape = (160, 576)
+    data_dir = './data'
+    runs_dir = './runs'
+    tests.test_for_kitti_dataset(data_dir)
+
+    # Download pretrained vgg model
+    helper.maybe_download_pretrained_vgg(data_dir)
+
+    # OPTIONAL: Train and Inference on the cityscapes dataset instead of the Kitti dataset.
+    # You'll need a GPU with at least 10 teraFLOPS to train on.
+    #  https://www.cityscapes-dataset.com/
+
+    with tf.Session() as sess:
+        # Create function to get batches
+        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
+
+        # OPTIONAL: Augment Images for better results
+        #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
+
+        # TODO: Build NN using load_vgg, layers, and optimize function
+        input_image, keep_prob, vgg_layer3_out, vgg_layer4_out, vgg_layer7_out = load_vgg(sess, os.path.join(data_dir, 'vgg'))
+        layers_output = layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
+        correct_label = tf.placeholder(tf.float32, [None, None, None, num_classes])
+        learning_rate = tf.placeholder(tf.float32)
+        logits, train_op, cross_entropy_loss = optimize(layers_output, correct_label, learning_rate, num_classes)
+
+        # TODO: Train NN using the train_nn function
+        train_nn(sess, 5, 2, get_batches_fn, train_op, cross_entropy_loss, input_image, correct_label, keep_prob, learning_rate)
+
+        # TODO: Save inference data using helper.save_inference_samples
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+
+
+if __name__ == '__main__':
+    run()
